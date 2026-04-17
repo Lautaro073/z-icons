ac96d43a0b897a707cf0d45df73c18d22058e7f1
Lautaro073 <lautarojimenez02@gmail.com>
2026-04-17 17:03:42 -0300

    refactor(features): add public barrels for features and lib/components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from "./common";
+export * from "./controllers";
+export * from "./guards";
+export * from "./ui";
+
+// Top-level barrel for components. Import from `@/components` in code.
---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+export * from "./api";
+export * from "./fontawesome";
+export * from "./preferences";
+export * from "./query";
+export * from "./server";
+export * from "./utils";
+
+// Barrels: keep light and only re-export public adapters/services.
